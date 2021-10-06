fed3fb56c25b98ff343623bba02d3364ebad3922
Felipe <felipe.mmda@mmda.com.br>
2021-10-06 16:18:17 -0300

    adding analytics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
+import ReactGA from 'react-ga';
 
+ReactGA.initialize('G-Y217MX4SM5');
 function App() {
   return (
+    
     <div className="App">
       <a
         href="https://pt.wikipedia.org/wiki/Valorant"
