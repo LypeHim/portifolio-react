b02a4c5527e34b11e4f684a56348f882402d610a
Felipe <felipe.mmda@mmda.com.br>
2021-10-07 11:42:20 -0300

    adding new link

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
 import ReactGA from 'react-ga';
 
-ReactGA.initialize('G-Y217MX4SM5');
 function App() {
+  ReactGA.initialize('G-Y217MX4SM5');
   return (
     
     <div className="App">
-      <a
-        href="https://pt.wikipedia.org/wiki/Valorant"
-        target="_blank"
-        rel="noreferrer"
-        >
-        Link externo teste Analytics
-      </a>
+      <ul>
+        <li>
+            <a
+            href="https://pt.wikipedia.org/wiki/Valorant"
+            target="_blank"
+            rel="noreferrer"
+            title="link externo 1"
+            >
+            Link externo teste Analytics
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://pt.wikipedia.org/wiki/Valorant"
+            target="_blank"
+            rel="noreferrer"
+            title="link externo 2"
+            >
+            Segundo link de teste
+          </a>
+        </li>      
+      </ul>
+      
       <p>teste</p>
     </div>
   );
